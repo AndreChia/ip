bf845da6ddbfdc444e7a4815213ea36c07f4fdbd
AndreChia <41582334+AndreChia@users.noreply.github.com>
2025-01-23 04:05:47 +0800

    Added Echo

--- src/main/java/Phone.java
+++ src/main/java/Phone.java
@@ -1,10 +1,19 @@
-public class Duke {
+import java.util.Scanner;
+
+public class Phone {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm PHONE");
+        System.out.println("What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String command;
+
+        while (true) {
+            command = sc.nextLine();
+            System.out.println(command);
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+        }
     }
 }
