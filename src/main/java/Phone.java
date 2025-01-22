878e04a985fe4de10e7aace5bbb3cd9cdeef575d
AndreChia <41582334+AndreChia@users.noreply.github.com>
2025-01-23 02:51:04 +0800

    Added Phone.java

---
+++ src/main/java/Phone.java
@@ -0,0 +1,6 @@
+public class Phone {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm PHONE\nWhat can I do for you?\nBye. Hope to see you again soon!");
+        System.out.println("");
+    }
+}
